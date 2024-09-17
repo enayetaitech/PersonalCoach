3471ea25e70b36beaacdb01819f7d9e5212c039c
enayetaitech <mdrehman@365aitech.com>
2024-09-17 17:28:12 +0600

    partial area name search and price lower than given search is done

--- airtableRoutes.js
+++ airtableRoutes.js
@@ -10,7 +10,7 @@ router.post("/airtable", async (req, res) => {
 
 
     // Make request to make.com webhook
-    const response = await axios.post(`https://hook.eu2.make.com/ymcronhxemxx3eboimq4uxxqo76p8o8t`, {
+    const response = await axios.post(`https://hook.eu2.make.com/a4b097fnuhh5xnp2oz3kux70bgxjm2eq`, {
       area, propertyType, bathroom, bedroom, budget
     });
     
@@ -22,6 +22,27 @@ router.post("/airtable", async (req, res) => {
     res.status(500).send('Error')
   }
 })
+
+// first api with 5 fixed req.body
+// router.post("/airtable", async (req, res) => {
+//   try {
+//     const { area , propertyType, bathroom, bedroom, budget} = req.body;
+//     console.log('Data received from vapi at', new Date(), 'Area:', area, 'Property Type:', propertyType, 'Bathroom:', bathroom, 'Bedroom:', bedroom, 'Budget:', budget);
+
+
+//     // Make request to make.com webhook
+//     const response = await axios.post(`https://hook.eu2.make.com/ymcronhxemxx3eboimq4uxxqo76p8o8t`, {
+//       area, propertyType, bathroom, bedroom, budget
+//     });
+    
+//     console.log('response received from make.com at', new Date(), 'response', response.data)
+//     // Return the data from the webhook response
+//     res.json(response.data)
+
+//   } catch (error) {
+//     res.status(500).send('Error')
+//   }
+// })
 
 
 module.exports = router;
